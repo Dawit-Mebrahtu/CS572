a99fb1d672f262fb33951e724c03273b09e75819
Dawit Mebrahtu <dawitaraya7@gmail.com>
2019-01-15 19:16:06 -0600

    Removed unnecessary commented code

--- lab2/evenOdd.js
+++ lab2/evenOdd.js
@@ -13,13 +13,7 @@ Array.prototype.odd = function(num){
     });
 };
 
-// function async(){
-//     [1, 2, 3, 4, 5, 6, 7, 8].even();
-//     [1, 2, 3, 4, 5, 6, 7, 8].odd();
-// }
-
 console.log('start');
-//setTimeout(async, 0);
 [1, 2, 3, 4, 5, 6, 7, 8].even();
 [1, 2, 3, 4, 5, 6, 7, 8].odd();
 console.log('end');
